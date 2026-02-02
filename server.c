11b7f90eabb9206f5a82496b610a7635ed4f7ed3
sapuplanta <jevaanraj17@gmail.com>
2026-02-02 16:21:08 +0800

    define game structure and player parameters in server implementation

--- server.c
+++ server.c
@@ -4,4 +4,14 @@
 #include <sys/mman.h> //memory management declarations
 #include <time.h> //time functions
 
+#define PLAYERS 3   //number of players
+#define PATH_LEN 6  //length of the path
 
+// Shared Game Structure
+typedef struct {
+    int tileds[PATH_LEN]; //tiles on the path
+    int player_positions[PLAYERS]; //positions of players
+    int alive [PLAYERS]; //alive status of players
+    int turn; //current turn
+} Game; 
+Game *game; //pointer to shared game structure
