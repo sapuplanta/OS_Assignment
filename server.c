7ae85ee44be832cffd604c209c307d6ee4de779a
sapuplanta <jevaanraj17@gmail.com>
2026-02-02 16:15:12 +0800

    add includes for standard libraries in server implementation

---
+++ server.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h> //posix standard functions
+#include <sys/mman.h> //memory management declarations
+#include <time.h> //time functions
+
+
